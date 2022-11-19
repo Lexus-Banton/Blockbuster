de9269e0137afbe1bd068e23c27aa2b037f9ff67
Lex <106777728+Lexus-Banton@users.noreply.github.com>
2022-11-18 17:57:21 -0800

    tests passing for 3-7

--- exercise_5/exercise-5.sql
+++ exercise_5/exercise-5.sql
@@ -1,2 +1,11 @@
 -- find all film titles that an actor with the last_name Davis appears in
 -- Similar to exercise 3 but with actor, film_actor, film
+-- find all film titles that an actor with the last_name Davis appears in
+-- Similar to exercise 3 but with actor, film_actor, film
+Select
+film.title
+From
+film
+Left join film_actor on film_actor.film_id = film.film_id
+left join actor on actor.actor_id = film_actor.actor_id
+where actor.last_name = 'Davis'
--- exercise_6/exercise-6.sql
+++ exercise_6/exercise-6.sql
@@ -1,2 +1,12 @@
 -- find all film titles rented out by customers with the first_name Roberta
 -- Similar to exercise 5
+-- find all film titles rented out by customers with the first_name Roberta
+-- Similar to exercise 5
+select
+film.title
+from
+film
+Left Join inventory on inventory.film_id = film.film_id
+Left Join rental on rental.inventory_id = inventory.inventory_id
+Left Join customer on customer.customer_id = rental.customer_id
+Where customer.first_name = 'Roberta'
